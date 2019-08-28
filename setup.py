40a5c7d6e5425708f878d6c7654bd898f9fb798f
Alan Bacon <alan.bacon@bbc.co.uk>
2019-08-28 13:10:50 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='FnF',
+   version='0.0.1',
+   description='Library to work with Files \'n\' Folders',
+   author='Alan Bacon',
+   author_email='alan@bacontowers.co.uk',
+   packages=['FnF'],
+   install_requires=[],
+)
